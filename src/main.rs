4b44a3f72c4aae84f4593a34650746dd48793d85
daca <daca@itu.dk>
2022-04-03 15:44:21 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -66,6 +66,9 @@ const R_STATIC_MATERIAL: [u8; 32] = [
 const DEVEUI: [u8; 8] = [0x1, 1, 2, 3, 2, 4, 5, 7];
 const APPEUI: [u8; 8] = [0, 1, 2, 3, 4, 5, 6, 7];
 
+static mut FCNTUP: u16 = 0;
+static mut DEVADDR: [u8; 4] = [0,0,0,0];
+
 fn main() {
     println!("Hello, world!");
     let lora = setup_sx127x();
@@ -89,9 +92,6 @@ fn edhoc_handshake(mut lora: LoRa<Spi, OutputPin, OutputPin>) {
     let i_static_priv = StaticSecret::from(I_STATIC_MATERIAL);
     let i_static_pub = PublicKey::from(&i_static_priv);
     let r_static_pub = PublicKey::from(R_STATIC_MATERIAL);
-    // select a connection identifier
-    let i_c_i = [0x1].to_vec();
-    let i_c_i_cpy = i_c_i.clone();
     // create ehpemeral key material
     let mut r: StdRng = StdRng::from_entropy();
     let i_ephemeral_keying = r.gen::<[u8; 32]>();
@@ -116,7 +116,6 @@ fn edhoc_handshake(mut lora: LoRa<Spi, OutputPin, OutputPin>) {
     let poll = lora.poll_irq(Some(5000), &mut Delay);
     match poll {
         Ok(_size) => {
-            println!("with Payload: ");
             let buffer = lora.read_packet().unwrap(); // Received buffer. NOTE: 255 bytes are always returned
             println! {"{:?}", buffer}
             match buffer[0] {
@@ -125,7 +124,18 @@ fn edhoc_handshake(mut lora: LoRa<Spi, OutputPin, OutputPin>) {
                     //let (msg3, Msg4_Reciever) = 
                     match edhoc_third_message(msg.to_vec(), msg2_reciever, r_static_pub) {
                         Ok((msg3, Msg4_Reciever)) => {
-
+                            let (msg, len) = lora_send(msg3);
+                            let transmit = lora.transmit_payload_busy(msg, len);
+                            match transmit {
+                                Ok(packet_size) => println!("Sent packet with size: {:?}", packet_size),
+                                Err(_) => println!("Error"),
+                            }
+                        }
+                        Err(OwnOrPeerError::PeerError(x)) => {
+                            println!("Something went horrible wrong! {:?}", x)
+                        }
+                        Err(OwnOrPeerError::OwnError(x)) => {
+                            println!("TODO: Send error message")
                         }
                     }
                 }
@@ -143,10 +153,12 @@ fn edhoc_first_message(msg1_sender: PartyI<Msg1Sender>) -> (Vec<u8>, PartyI<Msg2
     msg1_sender.generate_message_1(METHOD_TYPE_I, SUITE_I).unwrap();
 
     println!("msg1 sent {:?}", msg1_bytes);
+    println!("msg1 len {:?}", msg1_bytes.len());
 
     // adding mtype
-    let mut payload1 = [0].to_vec();
-    payload1.extend(msg1_bytes);
+    //    let mut payload1 = [0].to_vec();
+    //    payload1.extend(msg1_bytes);
+    let payload1 = prepare_message(msg1_bytes, 0, true);
     (payload1, msg2_receiver)
 }
 
@@ -155,17 +167,26 @@ fn edhoc_third_message(
     msg2_receiver: PartyI<Msg2Receiver>,
     mut r_static_pub: PublicKey,
 ) -> Result<(Vec<u8>, PartyI<Msg4ReceiveVerify>), OwnOrPeerError> {
-    let devaddr = &msg2[1..5];
-    let msg2 = &msg2[5..];
+    println!("");
+    println!("msg2.len before removing devaddr {:?} ", msg2.len());
+    unsafe {
+        DEVADDR = msg2[0..4].try_into().unwrap();
+        println!("\nDevAdrr {:?}\n", DEVADDR);
+    }
+    let msg2 = remove_devaddr(msg2);
+    println!("msg2.len {:?} ", msg2.len());
     println!("msg2 {:?} ", msg2);
+
 
     // read from file, and check what key responds to r_kid
     // Needs to be used when verififying message2 instead of &r_static_pub.as_bytes()
     let (r_kid, ad_r, msg2_verifier) =
-        match msg2_receiver.unpack_message_2_return_kid(msg2.to_vec()) {
+        match msg2_receiver.unpack_message_2_return_kid(msg2) {
             Err(OwnOrPeerError::PeerError(s)) => return Err(OwnOrPeerError::PeerError(s)),
             Err(OwnOrPeerError::OwnError(b)) => {
+                println!("First attempt at dying");
                 //return Ok(b)
+                println!("{:?}", &b);
                 return Err(OwnOrPeerError::OwnError(b));
             }
             Ok(val) => val,
@@ -174,6 +195,7 @@ fn edhoc_third_message(
 
     let msg3_sender = match msg2_verifier.verify_message_2(&r_static_pub.as_bytes().to_vec()) {
         Err(OwnError(b)) => {
+            println!("Or here");
             //return Ok(b)},
             return Err(OwnOrPeerError::OwnError(b));
         }
@@ -195,6 +217,26 @@ fn edhoc_third_message(
     Ok((payload3, msg4_receiver_verifier))
 }
 
+fn remove_devaddr(msg: Vec<u8>) -> Vec<u8>{
+    msg[4..].to_vec()
+}
+
+fn prepare_message(msg: Vec<u8>, mtype: u8, first_msg: bool) -> Vec<u8>{
+    let mut buffer = Vec::new();
+    buffer.extend_from_slice(&mtype.to_be_bytes());
+    unsafe {
+        buffer.extend_from_slice(&FCNTUP.to_be_bytes());
+        FCNTUP += 1;
+    }
+    if !first_msg {
+        unsafe {
+            buffer.extend_from_slice(&DEVADDR);
+        }
+    }
+    buffer.extend_from_slice(&msg);
+    buffer
+}
+
 fn setup_sx127x() -> LoRa<Spi, OutputPin, OutputPin> {
     let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 8_000_000, Mode::Mode0).unwrap();
 
@@ -212,4 +254,13 @@ fn lora_send(message: Vec<u8>) -> ([u8; 255], usize) {
         buffer[i] = *byte;
     }
     (buffer, message.len())
+}
+
+fn hexstring(slice: &[u8]) -> String {
+    String::from("0x")
+        + &slice
+            .iter()
+            .map(|n| format!("{:02X}", n))
+            .collect::<Vec<String>>()
+            .join(", 0x")
 }
